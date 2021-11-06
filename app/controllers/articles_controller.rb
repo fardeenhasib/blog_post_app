e8d897c997cde4029ab0a6f9ddf8c48e4f861dcd
fardeenhasib <fardeenmozumder@gmail.com>
2021-11-06 20:05:07 +0000

    fixed new template (path, error messages and vaidation)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,13 @@ class ArticlesController < ApplicationController
    def create
       #render plain: params[:article].inspect
       @article = Article.new(article_params)
-      @article.save
-      redirect_to articles_show(@article)
+      if @article.save
+         #do sth
+         flash[:notice] = "Article was succesfully created"
+         redirect_to article_path(@article)
+      else
+         render 'new'
+      end
    end
    
    private
